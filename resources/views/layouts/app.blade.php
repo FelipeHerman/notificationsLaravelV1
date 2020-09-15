7bfb2b993622dd4e7f77bc74bcab2498d40cb4fd
Felipe Herman <j_felipe05@hotmail.com>
2020-09-15 15:43:43 -0500

    Autentificando los mensajes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,8 @@
                         @else
                             <li><a href="/home">Enviar mensaje</a></li>
                             <li>
-                                <a href="{{ route('notifications.index') }}">Notificaciones 
+                                <a href="{{ route('notifications.index') }}">
+                                    Notificaciones 
                                     @if($count = Auth::user()->notifications->count())
                                         <span class="badge">{{ $count }}</span>
                                     @endif
