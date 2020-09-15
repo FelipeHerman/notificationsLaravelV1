6fee22f081459b6d35d8b3374434913095ab2032
Felipe Herman <j_felipe05@hotmail.com>
2020-09-15 15:49:06 -0500

    Creacion de vista para las notificaciones

---
+++ resources/views/notifications/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Notificaciones</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <h2>No leídas</h2>
+            </div>
+
+            <div class="col-md-6">
+                <h2>Leídas</h2>
+            </div>
+        </div>
+    </div>
+@endsection
