1a64ddd7a463c7e6ad7e50099c6f966ddbd94272
Felipe Herman <j_felipe05@hotmail.com>
2020-09-15 09:46:47 -0500

    Init project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Enviar mensaje</div>
+                <form method="POST" action="{{ route('messages.store') }}">
+                    {{ csrf_field() }}
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="form-group">
+                            <select name="recipient_id" class="form-control">
+                                <option value="">Selecciona el usuario</option>
+                                @foreach($users as $user)
+                                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                            
+                        <div class="form-group">
+                            <textarea name="body" class="form-control" placeholder="Escribe aquí tu mensaje"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <button class="btn btn-primary btn-block">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
